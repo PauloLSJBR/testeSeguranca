483f7cc0c464007d7dc1ccdee8a807acafddf2dc
PauloLSJBR <37719839+PauloLSJBR@users.noreply.github.com>
2022-03-28 12:09:11 -0300

    commit inicial

---
+++ src/main/java/com/example/testeSeguraca/TesteSeguracaApplication.java
@@ -0,0 +1,14 @@
+package com.example.testeSeguraca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Enable
+public class TesteSeguracaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteSeguracaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testeSeguraca/TesteSeguracaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testeSeguraca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteSeguracaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
